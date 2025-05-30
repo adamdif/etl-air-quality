4729977817bf82ec140cdd4bb032de76b80cee31
Adam <adam.elmoutaoukil@ensiie.eu>
2025-05-31 01:10:09 +0200

    Working extract.py: writes data on json

--- extract.py
+++ extract.py
@@ -4,7 +4,7 @@ import os
 
 # Liste des pays à extraire (tu peux changer)
 countries = ["FR", "DE", "IT"]
-url = "https://api.openaq.org/v3/measurements"
+url = "https://api.openaq.org/v3/locations"
 
 # Dossier de sortie
 output_path = "data/raw_data.json"
@@ -21,10 +21,12 @@ print("📡 Récupération des données depuis OpenAQ...")
 
 headers = {
     "accept": "application/json",
-    "x-api-key": os.getenv("OPENAQ_API_KEY")
+    "X-API-Key": os.getenv('OPENAQ_API_KEY')
 }
 
 response = requests.get(url, params=params, headers=headers)
+
+print("🔑 Clé API utilisée :", os.getenv("OPENAQ_API_KEY"))
 if response.status_code == 200:
     data = response.json()
     with open(output_path, "w") as f:
@@ -32,3 +34,5 @@ if response.status_code == 200:
     print(f"✅ Données enregistrées dans {output_path}")
 else:
     print("❌ Erreur lors de l’appel API :", response.status_code)
+    
+
